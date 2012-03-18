6c6306afe3c204a1f38a52cbac4e45c0d988f45e
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-03-18 13:31:53 -0700

    changed config to allow assets to be compiles on the fly on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Secretconfessions::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #AZC Was changed to true to allow assets to be compiles on the fly on Heruko
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
