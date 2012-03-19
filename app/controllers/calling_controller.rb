574adfc82c6563d7e8688a231fc70aec4a294caa
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-03-19 00:53:36 -0700

    simplified tropo hello world

--- app/controllers/calling_controller.rb
+++ app/controllers/calling_controller.rb
@@ -3,20 +3,39 @@ class CallingController < ApplicationController
 
   post '/calling/index.json' do
 
-      v = Tropo::Generator.parse request.env["rack.input"].read
-      t = Tropo::Generator.new
+    tropo = Tropo::Generator.new do
+      say 'Hello World! Hello World! Hello World!'
+    end
+    tropo.response
 
-      if v[:session][:initial_text].to_i > 0
-          t.ask :name => 'initial_text', :choices => { :value => "[ANY]"}
-          session[:areacode] = v[:session][:initial_text]        
-      else
-          t.say "Welcome to Secret Confessions.  You have 30 seconds to leave your dirt.  Press any button or hang up when you are done."
-      end
-      # t.on :event => 'continue', :next => '/calling/done.json'
-      t.hangup
-      t.response
   end
 
+  # post '/calling/index.json' do
+  # 
+  #     v = Tropo::Generator.parse request.env["rack.input"].read
+  #     t = Tropo::Generator.new
+  # 
+  #     if v[:session][:initial_text].to_i > 0
+  #         t.ask :name => 'initial_text', :choices => { :value => "[ANY]"}
+  #         session[:areacode] = v[:session][:initial_text]        
+  #     else
+  #         t.say "Welcome to Secret Confessions.  You have 30 seconds to leave your dirt.  Press any button or hang up when you are done."
+  #     end
+  #     # t.on :event => 'continue', :next => '/calling/done.json'
+  #     t.hangup
+  #     t.response
+  # end
+  
+  ####################### AZC
+  # answer
+  # 
+  # ## record a message.  Play the beep, caller must speak within 10 seconds.  
+  # ## Allow 7 seconds of silence and up to 30 seconds of recording.
+  # 
+  # result = record "Hello.  Thanks for calling.  Leave your message at the beep.",
+  #           { :beep => true, :timeout => 10, :silenceTimeout => 7, :maxTime => 30 }
+  ####################### AZC  				
+    				
   # post '/calling/done.json' do
   # 
   #     t = Tropo::Generator.new
