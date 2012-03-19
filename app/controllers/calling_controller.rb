2e8c5abe00f4341751a404d61f66bf12a926ed34
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-03-19 16:04:48 -0700

    changed route

--- app/controllers/calling_controller.rb
+++ app/controllers/calling_controller.rb
@@ -1,7 +1,7 @@
 class CallingController < ApplicationController
 
   def hello
-    post '/calling/index.json' do
+    post '/index.json' do
 
       tropo = Tropo::Generator.new do
         say 'Hello World! Hello World! Hello World!'
