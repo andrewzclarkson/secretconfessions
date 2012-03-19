114350b0c11d42db8d7a48bef3355b6a32d571fd
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-03-19 16:26:43 -0700

    made a change 2

--- app/controllers/calling_controller.rb
+++ app/controllers/calling_controller.rb
@@ -1,7 +1,7 @@
 class CallingController < ApplicationController
 
   def hello
-    post '/incoming.json' do
+#    post '/incoming.json' do
 
       tropo = Tropo::Generator.new do
         say 'Hello World! Hello World! Hello World!'
