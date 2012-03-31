58cdfeeefd03cc67964de01b4b2995c08700aedd
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-03-30 20:34:31 -0700

    changed wording on say

--- app/controllers/calling_controller.rb
+++ app/controllers/calling_controller.rb
@@ -4,13 +4,9 @@ class CallingController < ApplicationController
   # Route to this method => match 'calling/incomingCall' => 'calling#incomingCall', :via => [:get, :post]
   def incomingCall
     
-    areacode_regex = /^\d\d\d/
-    
     incomingPhoneNumber = params[:session][:from][:id]
-    puts incomingPhoneNumber
     
     incomingAreaCode = incomingPhoneNumber[0..-8]
-    puts incomingAreaCode
     
     @confession = Confession.create(:areacode => incomingAreaCode)
     
@@ -18,7 +14,7 @@ class CallingController < ApplicationController
      
     t = Tropo::Generator.new
       
-    t.say(:value => "I am the king of the world, Scott.  I am the internet and I have all of your information, forever.")
+    t.say(:value => "Welcome to Secret Confessions. You have 30 seconds to make your confession and the recording will be posted anonymously to our site. Go for it.")
       
     render :text => t.response
    end
